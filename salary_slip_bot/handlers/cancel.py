f120ae63cc221065aec75586f89f94abfd1f9625
Denis White <hexagon-wormier0d@icloud.com>
2024-09-09 16:00:50 +0700

    Next

--- salary_slip_bot/handlers/cancel.py
+++ salary_slip_bot/handlers/cancel.py
@@ -8,10 +8,5 @@ cancel_router = Router()
 
 @cancel_router.message(StateFilter('*'), F.text.casefold() == "отмена")
 async def cancel_handler(message: Message, state: FSMContext) -> None:
-
-    #current_state = await state.get_state()
-    #if current_state is None:
-    #    return
-
     await state.clear()
     await message.answer("Действия отменены", reply_markup=main_menu())
