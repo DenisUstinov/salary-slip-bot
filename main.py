6dce9653a44bfd998603c9249b785a42820fe664
Denis White <hexagon-wormier0d@icloud.com>
2024-09-11 07:52:01 +0700

    Next

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 import asyncio
 import logging
-import sys
-import ssl
-from aiohttp import web
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
-from aiogram.types import FSInputFile
+from aiogram import types
+from aiogram.filters import CommandStart
 
-from salary_slip_bot.config import (
-    BOT_TOKEN, ALLOWED_UPDATES, USER_ID_LIST, CHAT_TYPE_LIST, ENV,
-    SELF_SSL, DOMAIN, WEBHOOK_PATH, EXTERNAL_PORT, BASE_WEBHOOK_URL, 
-    WEB_SERVER_HOST, WEB_SERVER_PORT, WEBHOOK_SECRET, WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV
-)
+from salary_slip_bot.keyboards.reply import main_menu
+from salary_slip_bot.database.sqlite import init_db
+
+from salary_slip_bot.config import BOT_TOKEN, ALLOWED_UPDATES, USER_ID_LIST, CHAT_TYPE_LIST
+
 from salary_slip_bot.filters.chat_types import ChatTypeFilter
 from salary_slip_bot.filters.user_id import UserIdFilter
 from salary_slip_bot.handlers.cancel import cancel_router
-from salary_slip_bot.handlers.commands import commands_router
 from salary_slip_bot.handlers.attendance import attendance_router
 from salary_slip_bot.handlers.expenses import expenses_router
 from salary_slip_bot.handlers.settings import settings_router
@@ -28,14 +24,22 @@ from salary_slip_bot.handlers.deleter import deleter_router
 # Инициализация диспетчера событий
 dp: Dispatcher = Dispatcher()
 
-# Применение фильтров к сообщениям: фильтр по типу чата и по ID пользователей
+# Применение фильтров к сообщениям
 dp.message.filter(ChatTypeFilter(CHAT_TYPE_LIST))
 dp.message.filter(UserIdFilter(USER_ID_LIST))
 
-# Добавление маршрутизаторов для обработки команд и сообщений
+@dp.message(CommandStart())
+async def command_start_handler(message: types.Message) -> None:
+    user_id = message.from_user.id
+    await init_db(user_id)
+    await message.answer(
+        f"Привет, {message.from_user.full_name}!",
+        reply_markup=main_menu()
+    )
+
+# Добавление маршрутизаторов
 dp.include_routers(
     cancel_router,
-    commands_router,
     attendance_router,
     expenses_router,
     settings_router,
@@ -44,77 +48,19 @@ dp.include_routers(
     deleter_router
 )
 
-async def on_startup(bot: Bot) -> None:
-    if SELF_SSL:
-        # Если используешь самоподписанный сертификат, отправь его в Telegram
-        await bot.set_webhook(
-            f"{BASE_WEBHOOK_URL}{WEBHOOK_PATH}",
-            certificate=FSInputFile(WEBHOOK_SSL_CERT),
-            secret_token=WEBHOOK_SECRET,
-        )
-    else:
-        # Установка вебхука для обычного домена
-        await bot.set_webhook(f"{BASE_WEBHOOK_URL}{WEBHOOK_PATH}", secret_token=WEBHOOK_SECRET)
-
-async def on_shutdown(bot: Bot) -> None:
-    # Удаление вебхука при завершении работы бота
-    await bot.delete_webhook()
-
-def setup_webhook(app, dp, bot):
-    # Создание экземпляра обработчика запросов для вебхуков
-    webhook_requests_handler = SimpleRequestHandler(
-        dispatcher=dp,
-        bot=bot,
-        secret_token=WEBHOOK_SECRET,
-    )
-    # Регистрация обработчика вебхуков на маршруте
-    webhook_requests_handler.register(app, path=WEBHOOK_PATH)
-    # Настройка приложения aiohttp с хендлерами бота
-    setup_application(app, dp, bot=bot)
-
 async def main() -> None:
     bot: Bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
-    if ENV == "LOCAL":
-        # Удаление вебхука и работа с long-polling
-        await bot.delete_webhook(drop_pending_updates=True)
-        await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
-    else:
-        # Настройка вебхуков для окружения
-        dp.startup.register(on_startup)
-        dp.shutdown.register(on_shutdown)
-
-        # Создание aiohttp приложения
-        app = web.Application()
-        setup_webhook(app, dp, bot)
-
-        if SELF_SSL:
-            context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-            context.load_cert_chain(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV)
-            web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT, ssl_context=context)
-        else:
-            web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT)
+    logging.info("Starting in long-polling mode")
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
 
 if __name__ == "__main__":
-    if ENV == "LOCAL":
-        # Логирование для локальной среды
-        logging.basicConfig(
-            level=logging.DEBUG,
-            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-            handlers=[
-                logging.StreamHandler(sys.stdout),
-                logging.FileHandler('bot.log', mode='a')
-            ]
-        )
-    else:
-        # Логирование для продакшена
-        logging.basicConfig(
-            level=logging.INFO,
-            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-            handlers=[
-                logging.FileHandler('bot.log', mode='a')
-            ]
-        )
-
-    # Запуск основной асинхронной функции
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        handlers=[
+            logging.FileHandler('bot.log', mode='a')
+        ]
+    )
     asyncio.run(main())
--- salary_slip_bot/config.py
+++ salary_slip_bot/config.py
@@ -10,20 +10,6 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 # Окружение (LOCAL или PROD)
 ENV = os.getenv("ENV", "PROD")
 
-# Вебхуки
-SELF_SSL = os.getenv("SELF_SSL", "False").lower() == "true"
-DOMAIN = os.getenv("DOMAIN_IP") if SELF_SSL else os.getenv("DOMAIN_NAME")
-WEBHOOK_PATH = "/" + os.getenv("PROJECT_NAME")
-EXTERNAL_PORT = int(os.getenv("EXTERNAL_PORT", 8443))
-BASE_WEBHOOK_URL = f"https://{DOMAIN}:{EXTERNAL_PORT}" if SELF_SSL else f"https://{DOMAIN}"
-WEB_SERVER_HOST = "127.0.0.1" if not SELF_SSL else DOMAIN
-WEB_SERVER_PORT = 8080 if not SELF_SSL else EXTERNAL_PORT
-WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET", "my-secret")
-
-# SSL-сертификаты для работы с вебхуками (если используется самоподписанный сертификат)
-WEBHOOK_SSL_CERT = os.getenv("WEBHOOK_SSL_CERT", "../SSL/yourdomain.self.crt")
-WEBHOOK_SSL_PRIV = os.getenv("WEBHOOK_SSL_PRIV", "../SSL/yourdomain.self.key")
-
 # Настройки обновлений
 ALLOWED_UPDATES = [
     'message',
